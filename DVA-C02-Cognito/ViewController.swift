b6de9256d1c2912658a56976f17ec0de43fdee56
QuangHo <quangho@wee.vn>
2026-03-02 16:04:11 +0700

    Initial Commit

---
+++ DVA-C02-Cognito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DVA-C02-Cognito
+//
+//  Created by QuangHo on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
